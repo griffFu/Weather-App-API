34c3e686a78bce544cfface8b38e2783cc0a6dbc
griffFu <gfulto.9@gmail.com>
2022-09-18 19:21:55 -0500

    worked on the try catch and fixed some of the styling

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //https://api.openweathermap.org/data/2.5/weather?q=London&APPID=7ede2840c992ead1534f5115013dd6f1
 //Example of a call for london
 
-//1.) Retreive all of the variables I would like to recieve
-//2.) Look at other projects to see what they have included
-//3.) Create the logic for displaying different icons and diff animations for the website, i.e. background
-//3.) Create the try/catch logic
-//4.) If it doesnt work, then print a message to the user maybe??
+//1.) try/catch logic for invalid input
+//2.) have london be default version
+//3.) let input box have london in gray text before you enter anything
+//4.) 
+
 
 
 
@@ -18,11 +18,11 @@ let feelsLike = document.getElementById('feelsLike');
 
 
 
-let response = document.getElementById("search");
+let responseInput = document.getElementById("search");
 
 
 // event listener thats waiting for user to click enter, must be in input box though
-response.addEventListener("keydown", function (e) {
+responseInput.addEventListener("keydown", function (e) {
     if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
         getTemp();
     }
@@ -35,6 +35,7 @@ response.addEventListener("keydown", function (e) {
 async function getTemp(){
     let userInput = document.getElementById("search").value;
     // run the API
+    try{
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&APPID=7ede2840c992ead1534f5115013dd6f1`,{mode:'cors'});
     let weatherData = await response.json();
 
@@ -48,9 +49,15 @@ async function getTemp(){
     //populating the HTML
     forecast.innerHTML = weatherData['weather'][0]['description'];
     Atemp.innerHTML = kelvenToFahr(tempKel);
-    feelsLike.innerHTML += kelvenToFahr(tempKelFeels);
+    feelsLike.innerHTML = kelvenToFahr(tempKelFeels);
     humidity.innerHTML = responseHumidity + "%";
-    
+    } catch{
+        responseInput.setCustomValidity("Location non-existent")
+        responseInput.reportValidity();
+        renderLondon();
+        document.getElementById("search").value = '';
+    }
+
 }
 
 
@@ -64,6 +71,27 @@ function kelvenToFahr (enter){
 
 
 
+
+async function renderLondon() {
+    let londonInput = "London";
+    // run the API
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${londonInput}&APPID=7ede2840c992ead1534f5115013dd6f1`,{mode:'cors'});
+    let weatherData = await response.json();
+
+    //setting the raw ouptput to variables and fixing input
+    let tempKel = weatherData.main.temp;
+    let tempKelFeels =  weatherData.main.feels_like;
+    let responseHumidity = weatherData.main.humidity;
+   
+    
+    
+    //populating the HTML
+    forecast.innerHTML = weatherData['weather'][0]['description'];
+    Atemp.innerHTML = kelvenToFahr(tempKel);
+    feelsLike.innerHTML = kelvenToFahr(tempKelFeels);
+    humidity.innerHTML = responseHumidity + "%";
+    
+}
 
 
 
